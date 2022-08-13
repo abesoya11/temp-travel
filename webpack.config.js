22d00c86461c3fc178f27530c4c1429220be82db
aditya <abesoya11@gmail.com>
2022-08-13 18:53:06 +0530

    fheel

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,19 @@
-const { Module } = require("webpack");
+const path = require('path');
+module.exports ={
+    entry:'./app/assets/scripts/app.js',
+    output: {
+        filename:'bundles.js',
+        path: path.resolve(__dirname, 'app')
+    },
+    mode: 'development',
+    watch:true,
+    module:{
 
-Module.exports = {
-    entry:'./app/assets/scripts/app.js'
+        rules:[
+            {
+                test:/\.css$/i,
+                use:['style-loader','css-loader']
+            }
+            ]
+            }
 }
